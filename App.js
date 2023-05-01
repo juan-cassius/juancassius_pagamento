21f0a72410d8e59f432599802ad9d36b4102cf3a
Juan Cassius <cassjuan@hotmail.com>
2023-05-01 16:35:23 -0300

    navigation instalado, instalando fontes

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PaymentScreen from './screens/PaymentScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Pagamento"
+          component={PaymentScreen}
+          options={{
+            title: 'Pagamento',
+            headerStyle: {
+              backgroundColor: '#006557',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: '200',
+            }
+          }
+          }
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
